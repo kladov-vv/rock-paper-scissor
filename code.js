344157b130a5e05419be2518cfcbe97ad629b921
kladov-vv <kladov.vv@yandex.com>
2024-09-07 14:29:33 +1000

    Add getComputerChoice function, what generetes random computer choice

--- code.js
+++ code.js
@@ -1,1 +1,28 @@
-console.log('Hello world!')
+console.log('Hello world!');
+
+
+// this function generates computer choice in the game
+function getComputerChoice() {
+    // this part generates random number between 1 and 3
+    const minCeil = Math.ceil(1);
+    const maxFloor = Math.floor(4);
+    let randomNum = ( Math.floor(Math.random() * (maxFloor - minCeil) + minCeil) );
+
+    // this switch statement returns a computer choice depending on the generated number
+    switch(randomNum) {
+        case 1:
+            return 'rock'; 
+            console.log('rock')
+        break;
+
+        case 2:
+            return 'paper'; 
+            console.log('paper')
+        break;
+
+        case 3:
+            return 'scissors';
+            console.log('scissors') 
+        break;
+    }
+}
