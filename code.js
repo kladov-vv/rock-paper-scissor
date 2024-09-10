68340194de3c5d0e0bc2e1e782adc41785fec939
kladov-vv <kladov.vv@yandex.com>
2024-09-10 18:09:13 +1000

    make player answer case insensitive

--- code.js
+++ code.js
@@ -2,41 +2,70 @@ console.log('Hello world!');
 let computerScore = 0;
 let humanScore = 0;
 
-// this function generates random computer's choice for the game
+// this function generates a computer choice
 function getComputerChoice() {
-    // this part generates random number between 1 and 3
+    // this part generates a random number between 1 and 3
     const minCeil = Math.ceil(1);
     const maxFloor = Math.floor(4);
     let randomNum = ( Math.floor(Math.random() * (maxFloor - minCeil) + minCeil) );
 
-    // this switch statement returns a computer's choice depending on the generated earlier number
+    // this switch statement returns a computer choice depending on the generated random number
     switch(randomNum) {
         case 1:
             return 'rock';
-            // console.log('rock')
-        break;
+            // console.log('Rock');
+            break;
 
         case 2:
             return 'paper'; 
-            // console.log('paper')
-        break;
+            // console.log('Paper');
+            break;
 
         case 3:
             return 'scissors';
-            // console.log('scissors') 
-        break;
+            // console.log('Scissors');
+            break;
     }
 }
 
-// this function asks player for a choice
+// this function asks player for a player choice
 function getHumanChoice() {
-    let playersChoice = prompt('Make a choice: rock, paper or scissors');
+    let humanChoice = prompt('Make a choice: Rock, Paper or Scissors');
+    // this part makes answer case insensitive
+    humanChoice = humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1).toLowerCase();
+    console.log(humanChoice);
 
-    if (playersChoice === 'rock' || playersChoice === 'paper'|| playersChoice === 'scissors') {
-        return playersChoice;
+    if (humanChoice === 'rock' || humanChoice === 'paper'|| humanChoice === 'scissors') {
+        return humanChoice;
         console.log(playersChoice);
     } else {
         console.log('Invalid choice');
         // returns 'undefined'
     }
 }
+
+function playRound(humanChoice, computerChoice) {
+//human Choice parameter should be case-insensitive
+    switch (humanChoice) {
+        case 'rock':
+            switch (computerChoice) {
+                case 'scissors':
+                    console.log('You win! Rock beats Scissors')
+                break;
+
+                case 'rock':
+                    console.log('Tie')
+                break;
+
+                case 'paper':
+                    console.log('You lose! Paper beats Rock')
+                break;
+            }
+        break;
+
+        
+    }
+
+}
+// playRound( getHumanChoice(), getComputerChoice() );
+
