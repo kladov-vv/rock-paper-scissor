887fe3fe4d6eeb6a98ff746cb09d2c164caecff9
kladov-vv <kladov.vv@yandex.com>
2024-09-17 16:21:21 +1000

    Add more comments

--- code.js
+++ code.js
@@ -3,7 +3,7 @@ let computerScore = 0;
 let humanScore = 0;
 let roundTimer = 1;
 
-// this function generates a computer choice
+// this function generates a Computer choice
 function getComputerChoice() {
     // this part generates a random number between 1 and 3
     const minCeil = Math.ceil(1);
@@ -23,7 +23,7 @@ function getComputerChoice() {
     }
 }
 
-// this function asks player for a player choice
+// this function asks Human for a their choice
 function getHumanChoice() {
     let humanChoice = prompt('Make a choice: Rock, Paper or Scissors');
     // this part makes answer case insensitive
@@ -34,28 +34,35 @@ function getHumanChoice() {
     if (humanChoice === 'Rock' || humanChoice === 'Paper'|| humanChoice === 'Scissors') {
         return humanChoice;
     } else {
+        // this part loop until Human will choose valid option 
         console.log('Invalid choice');
         return getHumanChoice();
     }
 }
 // this function plays a single round of the game
 function playRound(humanChoice, computerChoice) {
+   // this part checks for tie and show message about it
     if (humanChoice === computerChoice) {
         return `Tie! Between ${computerChoice} and ${computerChoice}. 
         Score: You: ${humanScore} - Computer: ${computerScore}`;
     } else {
+        // this function called if Human win round
         function winRound() {
+            // adjust score
             humanScore = ++humanScore
+            // message
             return `You win! ${humanChoice} beats ${computerChoice}! 
             Score: You: ${humanScore} - Computer: ${computerScore}`;
         }
-        
+        // this function called if Human lose round
         function loseRound() {
+            // adjust score
             computerScore = ++computerScore
+            // message
             return `You lose! ${computerChoice} beats ${humanChoice}! 
             Score: You: ${humanScore} - Computer: ${computerScore}`;
         }
-        
+        // this switch statement calls winRound() or loseRound() depending on Human and Computer choices
         switch(humanChoice) {
             case 'Rock':
                 switch(computerChoice) {
@@ -89,12 +96,14 @@ function playRound(humanChoice, computerChoice) {
 
 // this function plays a 5 round of the game
 function playGame() {
+    // this part checks for when game ends. If game still going, it loop function until game ends
     if (roundTimer < 6) {
         console.log (`Round ${roundTimer}`);
         let roundResult = playRound( getHumanChoice(), getComputerChoice() );
         console.log(roundResult)
         roundTimer = ++roundTimer;
         return playGame();
+    // this game shows message with result when game ends
     } else if (humanScore === computerScore) {
         return 'Tie!';
     } else if (humanScore > computerScore) {
