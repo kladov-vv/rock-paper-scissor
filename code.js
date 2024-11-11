294fd6de08b6d28e1cfe1e413ea429f2716d738c
kladov-vv <kladov.vv@yandex.com>
2024-11-11 19:11:58 +1000

    Add score

--- code.js
+++ code.js
@@ -6,27 +6,30 @@ let humanScore = 0;
 
 buttons.addEventListener('click', playGame);
 
+score.textContent = `You: ${humanScore} | Computer: ${computerScore}`
+
 function getComputerChoice() {
     let options = ['rock', 'paper', 'scissors'];
     return options[Math.floor(Math.random() * options.length)];
 }
 
-function displayResult() {
-    
-}
+// function displayResult() {}
 
 function playRound(humanChoice, computerChoice) {
     // check for a tie, than check if human win or lose
     if (humanChoice === computerChoice) {
         return `Tie! Between ${computerChoice} and ${computerChoice}.`;
+        // TIE
    } else if ( (humanChoice === 'rock' && computerChoice === 'scissors') || 
    (humanChoice === 'paper' && computerChoice === 'rock') || 
    (humanChoice === 'scissors' && computerChoice === 'paper') ) {
         humanScore = ++humanScore
         return `You win! ${humanChoice} beats ${computerChoice}!`;
+        // HUMAN WIN
    } else {
         computerScore = ++computerScore
         return `You lose! ${computerChoice} beats ${humanChoice}!`;
+        // HUMAN LOSE
    }
 }
 
