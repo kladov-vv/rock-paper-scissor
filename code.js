a7c086ef44797e5f321dc5f87643cc6a45cfe288
kladov-vv <kladov.vv@yandex.com>
2024-11-12 17:09:26 +1000

    Add gameOver function

--- code.js
+++ code.js
@@ -1,23 +1,23 @@
 const buttons = document.querySelector('.buttons');
 const score = document.querySelector('.score');
 const roundResult = document.querySelector('.round-result');
-const gameResult = document.createElement('div');
+// const gameResult = document.createElement('div');
 let computerScore = 0;
 let humanScore = 0;
 
 buttons.addEventListener('click', playGame);
 
 function getComputerChoice() {
-    let options = ['rock', 'paper', 'scissors'];
+    let options = ['ROCK', 'PAPER', 'SCISSORS'];
     return options[Math.floor(Math.random() * options.length)];
 }
 
 function playRound(humanChoice, computerChoice) {
     if (humanChoice === computerChoice) {
         return `Tie! Between ${computerChoice} and ${computerChoice}.`;
-    } else if ( (humanChoice === 'rock' && computerChoice === 'scissors') || 
-    (humanChoice === 'paper' && computerChoice === 'rock') || 
-    (humanChoice === 'scissors' && computerChoice === 'paper') ) {
+    } else if ( (humanChoice === 'ROCK' && computerChoice === 'SCISSORS') || 
+    (humanChoice === 'PAPER' && computerChoice === 'ROCK') || 
+    (humanChoice === 'SCISSORS' && computerChoice === 'PAPER') ) {
         humanScore = ++humanScore
         return `You win! ${humanChoice} beats ${computerChoice}!`;
     } else {
@@ -30,14 +30,14 @@ function updateScore() {
     score.textContent = `You: ${humanScore} | Computer: ${computerScore}`
 }
 
-function displayResult(result) {
+function displayRoundResult(result) {
     if (humanScore === 5 || computerScore === 5) {
         if (humanScore > computerScore) {
             message = 'You win the game!'
         } else {
             message = 'You lose the game!'
         }
-        endGame();
+        gameOver();
     } else {
         message = result;
     }
@@ -45,14 +45,16 @@ function displayResult(result) {
 }
     
 
-function endGame() {
+function gameOver() {
+    buttons.removeEventListener('click', playGame);
     // gameResult
 }
 
 function playGame(event) {
     let choice = event.target.id;
+    choice = choice.toUpperCase();
     let result = playRound(choice, getComputerChoice());
     updateScore();
-    displayResult(result);
+    displayRoundResult(result);
     // console.log(result);
 }
